66df52daad0c8008b8b18ce69b879c605fc14f6e
koheihida <88849254+koheihida@users.noreply.github.com>
2021-12-03 15:52:23 +0900

    タグ付け機能実装

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -6,8 +6,15 @@
     <form class="card-body" action="{{ route('store')}}" method="POST">
         @csrf
         <div class="form-floating">
-            <textarea class="form-control" placeholder="ここにメモを入力" name="content" style="height: 100px"></textarea>
+            <textarea class="form-control mb-5" name="content" style="height: 100px" placeholder="新しいメモ"></textarea>
         </div>
+        @foreach($tags as $t)
+            <div class="form-check form-check-inline mb-3">
+                <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $t['id']}}" value="{{ $t['id']}}" >
+                <label class="form-check-label" for="{{ $t['id']}}">{{ $t['name'] }}</label>
+            </div>
+        @endforeach
+        <input type="text" class="form-control w-50 mb-5" name="new_tag" placeholder="新しいタグ">
         <button type="submit" class="btn btn-outline-secondary">保存</button>
     </form>
 </div>
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -5,16 +5,23 @@
     <div class="card-header">メモ編集
         <form class="card-body" action="{{ route('destroy')}}" method="POST">
             @csrf
-            <input type="hidden" name="memo_id" value="{{$edit_memo['id']}}">
+            <input type="hidden" name="memo_id" value="{{$edit_memo[0]['id']}}">
             <button type="submit">削除</button>
         </form>
     </div>
     <form class="card-body" action="{{ route('update')}}" method="POST">
         @csrf
-        <input type="hidden" name="memo_id" value="{{$edit_memo['id']}}">
+        <input type="hidden" name="memo_id" value="{{$edit_memo[0]['id']}}">
         <div class="form-floating">
-            <textarea class="form-control" placeholder="ここにメモを入力" name="content" style="height: 100px">{{ $edit_memo['content'] }}</textarea>
+            <textarea class="form-control" placeholder="ここにメモを入力" name="content" style="height: 100px">{{ $edit_memo[0]['content'] }}</textarea>
         </div>
+        @foreach($tags as $t)
+            <div class="form-check form-check-inline mb-3">
+                <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $t['id']}}" value="{{ $t['id']}}"  {{in_array($t[ 'id' ], $include_tags) ? 'checked' : '' }}>
+                <label class="form-check-label" for="{{ $t['id']}}">{{ $t['name'] }}</label>
+            </div>
+        @endforeach
+        <input type="text" class="form-control w-50 mb-5" name="new_tag" placeholder="新しいタグ">
         <button type="submit" class="btn btn-outline-secondary">更新</button>
     </form>
 </div>
