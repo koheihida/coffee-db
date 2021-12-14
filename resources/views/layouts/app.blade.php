f388ad4a78d11ef5049acc72010e2675853a3aa9
koheihida <88849254+koheihida@users.noreply.github.com>
2021-12-14 17:42:20 +0900

    README編集

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
