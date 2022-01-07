f4cb55094b3c595cfaa91daadc33a14a803109bd
koheihida <88849254+koheihida@users.noreply.github.com>
2022-01-07 14:37:40 +0900

    commentテーブルのカラムの記述

--- database/migrations/2022_01_07_051131_create_comments_table.php
+++ database/migrations/2022_01_07_051131_create_comments_table.php
@@ -13,9 +13,17 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create('comments', function (Blueprint $table)
+        {
+            $table->unsignedBigInteger('id' ,true);
+            $table->longText('content');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('memo_id');
+            $table->softDeletes();
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('memo_id')->references('id')->on('memos');
         });
     }
 
