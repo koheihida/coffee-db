300fce5d204fd26abe37f55b9fdef14f68bde279
koheihida <88849254+koheihida@users.noreply.github.com>
2022-01-12 14:46:53 +0900

    コメントアウトの削除

--- server/app/Http/Controllers/CommentsController.php
+++ server/app/Http/Controllers/CommentsController.php
@@ -10,22 +10,9 @@ use GuzzleHttp\Psr7\Request as Psr7Request;
 
 class CommentsController extends Controller
 {
-
-
     public function index()
     {
-
         $item = Memo::all();
-        // $memos = Memo::all();
-
-        // dd($memos);
-        // $memos = Memo::where('content',$request->keyword)->simplePaginate(13);
-        // $memos = Memo::orderBy('created_at', 'desc')->get();
-
-        // $memo_show = Memo::select('memos.*')
-        //     ->where('memos.id','=',$id)
-        //     ->whereNull('memos.deleted_at')
-        //     ->get();
 
         return view('open', compact('item'));
     }
@@ -51,30 +38,7 @@ class CommentsController extends Controller
 
     public function store(Request $request)
     {
-        // $memo_show = Memo::select('id', 'content')
-        // ->where('memos.id','=',$id)
-        // ->whereNull('memos.deleted_at')
-        // ->get();
-        // dd($memo_show);
-        // $memo_show = Memo::select('id', 'content');
-        // dd($memo_show);
-            // ->where('memos.id','=',$id)
-            // ->whereNull('memos.deleted_at')
-            // ->get();
-
         $comments = $request->all();
-
-        // ->where('memos.id','=',$id)
-        // ->get();
-
-
         Comment::insert(['content' => $comments['comment'], 'user_id' => \Auth::id(), 'memo_id' => $comments['memo_id']]);
-
-
-        // return view('comment', compact('comments'));
-        // return redirect()->route('comment')->with('success', '新規登録完了しました');
-        // $comment_model = new Comment();
-        //     // $memos = $memo_model -> getMyMemo();
-        // return view('comment_model', compact('memo_model'));
     }
 }
